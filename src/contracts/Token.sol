1a2af9c06526f0d4258b9b93bccf00352dab8324
dhruv354 <dhruvsin@iitk.ac.in>
2021-10-27 11:31:29 +0530

    added some basic test of smart contracts

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.4.17;
+
+pragma solidity >=0.5.1 ;
 
 contract Token{
     string public name = "Dhruv";
+    string public symbol = "DhruvToken";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+
+    constructor() public{
+        totalSupply = 1000000 * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+        return true;
+    }
 }
 
 
--- src/test/Token.test.js
+++ src/test/Token.test.js
@@ -1,17 +1,70 @@
+const Token = artifacts.require('Token')
+
+
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
 
-const Token = artifacts.require('./Token')
 
-contract('Token', (accounts) => {
+
+contract('Token', ([deployer, sender, receiver]) => {
+    const symbol = 'DhruvToken'
+    const decimals = '18'
+    const totalSupply = tokens(1000000)
+    const name = "Dhruv"
+    let token
+
+    beforeEach(async() => {
+        token = await Token.new()
+    })
+
     describe('deployment', () => {
         it('tracks the name', async() => {
             //read token name here
-            const token = await Token.new()
-            const name = await token.name()
-            name.should.equal('Dhruv')
+            const result = await token.name()
+            result.should.equal(name)
+        })
+
+        it('tacks the symbol', async() => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('tracks the decimals', async() => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('tracks the total supply', async() => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
+        })
+
+        it('assigns the total supply to the deployer', async() => {
+            const result = await token.balanceOf(deployer)
+            result.toString().should.equal(totalSupply)
+        })
+    })
+
+    describe('sending the tokens', () => {
+        it('tracks sending of tokens', async() => {
+            let balanceOf;
+            console.log("balance before tranfer of tokens");
+            balanceOf = await token.balanceOf(deployer);
+            console.log(balanceOf.toString());
+            balanceOf = await token.balanceOf(receiver);
+            console.log(balanceOf.toString());
+
+            //do transfer
+            await token.transfer(receiver, '1000000000000000000', { from: deployer });
+
+            //after transfer
+            console.log("balance after tranfer of tokens");
+            balanceOf = await token.balanceOf(deployer);
+            console.log(balanceOf.toString());
+            balanceOf = await token.balanceOf(receiver);
+            console.log(balanceOf.toString());
         })
     })
 })
