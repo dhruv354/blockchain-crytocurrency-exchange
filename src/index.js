5d66d5c618cb61e4ccc8eb30e87358afc4f46519
dhruv354 <dhruvsin@iitk.ac.in>
2021-11-18 17:48:05 +0530

    done basic refactoring and loaded token in App.js using Web3 library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Web3 from 'web3';
+// import 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
