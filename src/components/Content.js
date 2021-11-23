f49e636bbcf861e26e7378859cc1bb57229d2d2d
dhruv354 <dhruvsin@iitk.ac.in>
2021-11-23 09:43:59 +0530

    created individual navbar and content components

---
+++ src/components/Content.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+// import { accountSelector } from '../store/selectors'
+import { LoadAllOrders } from '../store/interactions';
+import exchange from '../abis/Exchange'
+import { exchangeSelector } from '../store/selectors';
+// exchangeSelector
+
+class Content extends Component {
+
+  componentWillMount(){
+    this.loadBlockchainContractData(this.props.dispatch)
+  }
+
+  async loadBlockchainContractData(dispatch){
+
+    await LoadAllOrders(this.props.exchange, dispatch)
+  }
+
+  render() {
+    // console.log(LoadAllOrders());
+    return (
+        <div className="content">
+        <div className="vertical-split">
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+        <div className="vertical">
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+        <div className="vertical-split">
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+        <div className="vertical">
+          <div className="card bg-dark text-white">
+            <div className="card-header">
+              Card Title
+            </div>
+            <div className="card-body">
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="/#" className="card-link">Card link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    exchange: exchangeSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(Content) 
+// export default Content
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,18 @@
-import { create } from "combined-stream";
-import { createLogger } from "redux-logger";
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createLogger } from 'redux-logger'
+import rootReducer from './reducers'
 
-// const store = createStore(rootReducer)
 const loggerMiddleware = createLogger()
 const middleware = []
 
-
+// For Redux Dev Tools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export default function configureStore(preloadedState){
-    return createStore(
-        rootReducer, 
-        preloadedState, 
-        composeEnhancers(applyMiddleware(...middleware, loggerMiddleware)))
+const store = function configureStore(preloadedState) {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middleware, loggerMiddleware))
+  )
 }
+export default store
